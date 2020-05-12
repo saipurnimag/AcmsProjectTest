bd7d092baef52c15bdc8fa3557ba2cb2b04471cb
saipurnimag <saipurnima@outlook.com>
2020-05-12 15:25:46 +0530

    order and seller added

---
+++ EventsConsumers/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsConsumers
+{
+    class Order
+    {
+        public string OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime PromisedShipDate { get; set; }
+        public DateTime PromisedDeliveryDate { get; set; }
+        public DateTime ActualShipDate { get; set; }
+        public DateTime ActualDeliveryDate { get; set; }
+        public string CancellationOrigin { get; set; }
+        public string CancellationReason { get; set; }
+        public bool isReturned { get; set; }
+
+    }
+    class Seller
+    {
+        public string SellerId { get; set; }
+    }
+}
